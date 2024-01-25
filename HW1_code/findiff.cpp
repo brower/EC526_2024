1670f8a585755357fe6298bdad687e116e131fe3
brower <brower@bu.edu>
2024-01-25 11:33:56 -0500

    New Code with Printf IO

--- HW1_code/findiff.cpp
+++ HW1_code/findiff.cpp
@@ -35,16 +35,32 @@ int main(int argc, char** argv)
 {
   int i;
   double h;
-  const double x = 1.0;
+  double x = 1.0;
 
-  cout << setprecision(15); // .. 10 points after the decimal.
+  // USE PRINTF WHICH HAS BETTER FORMAT CONTROL:
   
-  // Loop over 17 values of h: 1 to 10^(-16).
-  h = 1.0;
-  for (i = 0; i < 17; i++)
-  {
-    // Print stuff to graph
-  }
+  // Format to Plot Data   
+  printf("#     h                fwd                bwd                 central               fwd Error             bwd Error          ctl Error \n");
+  
+  for (h = 1; h >= pow(10, -16); h *= 0.1)  // Loops over 17 decreasing valuse of h
+    {
+      // Print h, the forward, backward, and central difference of sin(x) at 1 as well as the exact derivative cos(x) at 1.
+      double fwd = forward_diff(x, h);
+      double bck = backward_diff(x, h);
+      double ctl = central_diff(x, h);
+      double fwd_error = abs(fwd - derivative(x))/abs(derivative(x));
+      double  bck_error = abs(bck - derivative(x))/abs(derivative(x));
+      double ctl_error  = abs(ctl - derivative(x))/abs(derivative(x));	
+      printf("%20.16f  %20.16f  %20.16f  %20.16f %20.16f %20.16f %20.16f \n", h, fwd, bck,ctl , fwd_error, bck_error, ctl_error );
+    }
+  
+  /************** HELP WITH GNUPLOT  IF YOU USE IT ******************
+plot "diff.dat" u (log($1)):(log($5))  
+gnuplot> replot "diff.dat" u (log($1)):(log($6))  
+gnuplot> replot "diff.dat" u (log($1)):(log($7))  
+  ********************************************************/
+  
+
 
   return 0;
 }
